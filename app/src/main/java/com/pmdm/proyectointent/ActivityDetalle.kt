f6a911396dfc56107a8314f54280dd59a9d0e100
Alba <alba.perez.ce@gmail.com>
2019-12-06 21:36:59 +0100

    probando intents y activities colores

--- app/src/main/java/com/pmdm/proyectointent/ActivityDetalle.kt
+++ app/src/main/java/com/pmdm/proyectointent/ActivityDetalle.kt
@@ -8,18 +8,21 @@ import kotlinx.android.synthetic.main.activity_detalle.*
 class ActivityDetalle : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //crea la activity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
 
+        //obtiene el valor declarado en el main activity mediante su clave
         val valor = intent.getStringExtra("color")
 
+        //cambia la caja de texto conforme el valor de la variable
         tResultado.setText(valor)
 
-
+        //si el valor es amarillo...
         if (valor == "amarillo") {
-            tResultado.setTextColor(Color.YELLOW)
+            tResultado.setTextColor(Color.YELLOW) //cambia el texto a amarillo y escribe dicho nombre
         } else {
-            tResultado.setTextColor(Color.RED)
+            tResultado.setTextColor(Color.RED) //lo mismo pero con el color rojo
         }
     }
 }
